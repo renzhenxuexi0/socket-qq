ae8972368f013403dc195f821eb1ca8ae334a0bb
wuhaiyue <3084972826@qq.com>
2022-11-02 23:52:03 +0800

    第一个版本

--- src/main/java/com/qq/utils/DruidDataSourceFactory.java
+++ src/main/java/com/qq/utils/DruidDataSourceFactory.java
@@ -1,5 +1,8 @@
 package com.qq.utils;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.datasource.pooled.PooledDataSourceFactory;
+
 public class DruidDataSourceFactory extends PooledDataSourceFactory {
     public DruidDataSourceFactory(){
         this.dataSource = new DruidDataSource();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.qq.app.socktqq {
     requires javafx.controls;
     requires javafx.fxml;
     requires druid;
+    requires org.mybatis;
 
     exports com.qq;
     opens com.qq to javafx.fxml;
