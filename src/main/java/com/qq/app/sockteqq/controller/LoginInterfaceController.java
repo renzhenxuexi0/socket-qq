482c0a51a61da7731a9d37ece32da2d27d4134c2
wuhaiyue <3084972826@qq.com>
2022-11-02 23:05:46 +0800

    第一个版本

---
+++ src/main/java/com/qq/app/sockteqq/controller/LoginInterfaceController.java
@@ -0,0 +1,23 @@
+package com.qq.app.sockteqq.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginInterfaceController {
+    @FXML
+    private Button login;
+
+    @FXML
+    private Button register;
+
+    @FXML
+    void login_event(ActionEvent event) {
+
+    }
+
+    @FXML
+    void register_event(ActionEvent event) {
+
+    }
+}
