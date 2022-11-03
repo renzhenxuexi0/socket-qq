b64b4f302eab1d418eb64ba923f6aa6cbfd9d3fc
wuhaiyue <3084972826@qq.com>
2022-11-03 22:42:40 +0800

    第一个版本

--- server/src/main/java/com/server/mapper/UserMapper.java
+++ server/src/main/java/com/server/mapper/UserMapper.java
@@ -4,5 +4,9 @@ import com.server.pojo.User;
 
 public interface UserMapper {
 
+    /**
+     * 添加用户
+     * @param user
+     */
     void addUser(User user);
 }
---
+++ server/src/main/java/com/server/service/UserService.java
@@ -0,0 +1,13 @@
+package com.server.service;
+
+import com.server.pojo.User;
+
+public class UserService {
+    /**
+     * 实现用户注册功能
+     * @param user
+     */
+    public void userRegister(User user){
+
+    }
+}
