cc97030d526f170ee994dc0fe669a22b23a8f93a
wuhaiyue <3084972826@qq.com>
2022-11-03 11:35:47 +0800

    第一个版本

---
+++ server/src/main/java/com/server/mapper/UserMapper.java
@@ -0,0 +1,5 @@
+package com.server.mapper;
+
+public interface UserMapper {
+
+}
---
+++ server/src/main/java/com/server/pojo/User.java
@@ -0,0 +1,60 @@
+package com.server.pojo;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String accountNumber;
+    private String password;
+    private Integer login;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", login=" + login +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getLogin() {
+        return login;
+    }
+
+    public void setLogin(Integer login) {
+        this.login = login;
+    }
+}
