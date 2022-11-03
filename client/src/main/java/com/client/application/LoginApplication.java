58ed0768c2cb5ffbdc53f5262d3d648dd860d66b
wuhaiyue <3084972826@qq.com>
2022-11-03 11:24:39 +0800

    第一个版本

--- client/src/main/java/com/client/application/LoginApplication.java
+++ client/src/main/java/com/client/application/LoginApplication.java
@@ -1,5 +1,6 @@
 package com.client.application;
 
+import com.client.utils.StageManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,12 +11,15 @@ import java.util.Objects;
 
 public class LoginApplication extends Application {
 
+    private static final StageManager stageManager = new StageManager();
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/login.fxml")));
         Scene scene = new Scene(root);
         primaryStage.setTitle("登录界面");
         primaryStage.setScene(scene);
+        stageManager.addStage("登录界面", primaryStage);
         primaryStage.show();
     }
 
---
+++ client/src/main/java/com/client/utils/StageManager.java
@@ -0,0 +1,43 @@
+package com.client.utils;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Stage管理类
+ */
+public class StageManager {
+    private static final Map<String, Stage> stageMap = new HashMap<>();//存放所有的Stage实例
+
+    public Stage getStage(String name){
+        return stageMap.get(name);
+    }
+
+    public void closeStage(String name){
+        stageMap.get(name).close();
+    }
+
+    //实现Stage的跳转，从currentStage跳转到targetStage
+    public void jump(String currentStageName, String targetStageName){
+        stageMap.get(currentStageName).close();
+        stageMap.get(targetStageName).show();
+    }
+
+    public void release(String name){
+        stageMap.remove(name);
+    }
+
+    public void addStage(String title, Stage stage) throws IOException {
+        stageMap.put(title, stage);
+    }
+}
