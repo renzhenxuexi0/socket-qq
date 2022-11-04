eb88964b2aa2dcf5be8beda5a2adaf22e871b275
wuhaiyue <3084972826@qq.com>
2022-11-04 10:39:28 +0800

    第一个版本

--- server/src/main/java/com/server/service/UserService.java
+++ server/src/main/java/com/server/service/UserService.java
@@ -1,7 +1,5 @@
 package com.server.service;
 
-import com.server.pojo.User;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,45 +9,39 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class UserService {
-//    private static ThreadPoolExecutor pool = new ThreadPoolExecutor(2, 4,
-//            2, TimeUnit.SECONDS,
-//            new ArrayBlockingQueue<>(2), Executors.defaultThreadFactory(),
-//            new ThreadPoolExecutor.AbortPolicy());
+    private static final ThreadPoolExecutor pool = new ThreadPoolExecutor(2, 4,
+            2, TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(2), Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.AbortPolicy());
 
     /**
      * 服务端完成用户注册的操作
      */
-    public void userRegister() throws IOException {
-        ServerSocket serverSocket = new ServerSocket(6666);
-        Socket accept = serverSocket.accept();
-        InputStream inputStream = accept.getInputStream();
-        Reader reader = new InputStreamReader(inputStream);
-        BufferedReader bfr = new BufferedReader(reader);
-        String msg;
-        while ((msg = bfr.readLine()) != null) {
-            System.out.println(msg);
+    public void userRegister() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(6666);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                pool.execute(() -> {
+                    try {
+                        InputStream is = socket.getInputStream();
+                        Reader reader = new InputStreamReader(is);
+                        BufferedReader bfr = new BufferedReader(reader);
+                        String msg;
+                        while ((msg = bfr.readLine()) != null) {
+                            System.out.println(msg);
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-//        while (true) {
-//            pool.execute(() -> {
-//                try {
-//                    Socket socket = serverSocket.accept();
-//                    socket.getInputStream();
-//                    InputStream is = socket.getInputStream();
-//                    Reader reader = new InputStreamReader(is);
-//                    BufferedReader bfr = new BufferedReader(reader);
-//
-//                    String msg;
-//                    while ((msg = bfr.readLine()) != null) {
-//                        System.out.println(msg);
-//                    }
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            });
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         UserService userService = new UserService();
         userService.userRegister();
     }
