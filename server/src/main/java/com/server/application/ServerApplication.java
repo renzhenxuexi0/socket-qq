02964a85b540c39c7b8d74ba9923a59d7055fd9d
wuhaiyue <3084972826@qq.com>
2022-11-05 14:21:56 +0800

    完成简单的注册功能

--- server/src/main/java/com/server/application/ServerApplication.java
+++ server/src/main/java/com/server/application/ServerApplication.java
@@ -1,10 +1,12 @@
 package com.server.application;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.util.Objects;
 
@@ -15,6 +17,13 @@ public class ServerApplication extends Application {
         Scene scene = new Scene(root);
         primaryStage.setTitle("服务端");
         primaryStage.setScene(scene);
+        // 结束子线程
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                System.exit(0);
+            }
+        });
         primaryStage.show();
     }
 }
