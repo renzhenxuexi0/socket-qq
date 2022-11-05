cb243800f2d1f31e14a1215423118ef8392cf32a
wuhaiyue <3084972826@qq.com>
2022-11-05 16:17:35 +0800

    完成简单的注册功能

--- client/src/main/java/com/client/pojo/Code.java
+++ client/src/main/java/com/client/pojo/Code.java
@@ -9,12 +9,12 @@ public class Code {
     // 注册成功
     public static final Integer REGISTER_SUCCESS = 1002;
     // 注册失败
-    public static final Integer REGISTER_Fail = 1003;
+    public static final Integer REGISTER_FAIL = 1003;
     // 用户登录
     public static final Integer USER_LOGIN = 2001;
     // 登录成功
     public static final Integer LOGIN_SUCCESS = 2002;
     // 登录失败
-    public static final Integer LOGIN_Fail = 2003;
+    public static final Integer LOGIN_FAIL = 2003;
 }
 
