83a5d1d6c74a28ee2232ae0b3bdacab77dba7843
lenovo <1172627925@qq.com>
2022-11-07 17:12:22 +0800

    Changes

--- client/src/main/java/com/client/controller/DialogBoxController.java
+++ client/src/main/java/com/client/controller/DialogBoxController.java
@@ -1,4 +1,24 @@
 package com.client.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class DialogBoxController {
+
+    @FXML
+    private TextArea input;
+
+    @FXML
+    private Button send;
+
+    @FXML
+    void closeThePage(ActionEvent event) {
+
+    }
+
+
+    public void closeThePage(ActionEvent actionEvent) {
+    }
 }
