5a9ad889447ee3d383294a18bd0e56d51f2063ee
lenovo <1172627925@qq.com>
2022-11-13 17:02:49 +0800

    Changes

---
+++ client/src/main/java/com/client/view/DialogBoxView.java
@@ -0,0 +1,8 @@
+package com.client.view;
+
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
+
+@FXMLView(value = "fxml/dialogBox.fxml", encoding = "UTF-8", stageStyle = "DECORATED")
+public class DialogBoxView extends AbstractFxmlView {
+}
