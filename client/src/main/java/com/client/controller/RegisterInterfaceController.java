cc1918e5ac1261b820819c95186fa912ffa473f2
giv <opnmsl@gugoole.com>
2022-11-13 20:18:10 +0800

    注册界面与功能的完善

--- client/src/main/java/com/client/controller/RegisterInterfaceController.java
+++ client/src/main/java/com/client/controller/RegisterInterfaceController.java
@@ -1,16 +1,21 @@
 package com.client.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.client.ClientApp;
 import com.client.pojo.Code;
 import com.client.pojo.Result;
 import com.client.pojo.User;
 import com.client.service.UserService;
 import com.client.utils.DragUtil;
+import com.client.utils.ProgressStageUtil;
+import com.client.utils.UserMemory;
 import com.client.view.LoginView;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.validation.RequiredFieldValidator;
 import de.felixroske.jfxsupport.FXMLController;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -20,6 +25,7 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 import org.kordamp.ikonli.fontawesome.FontAwesome;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +33,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @FXMLController
+@Slf4j
 public class RegisterInterfaceController implements Initializable {
+    @Autowired
+    private ThreadPoolExecutor poolExecutor;
     @FXML
     public Button minWindow;
     @FXML
@@ -70,32 +80,62 @@ public class RegisterInterfaceController implements Initializable {
      */
     @FXML
     void submitButtonEvent() {
-        Result result = new Result();
-        result.setCode(Code.USER_REGISTER);
-        User user = new User();
+
         String account = accountInput.getText();
         String password = passwordInput.getText();
         String username = userNameInput.getText();
-        user.setAccount(account);
-        user.setPassword(password);
-        user.setUsername(username);
-        user.setLogin(0);
-        result.setObject(user);
-        Result result2 = userService.userRegister(result);
-        Alert alert;
+
 
         if (!"".equals(account) && !"".equals(password) && !"".equals(username)) {
-            if (Code.REGISTER_SUCCESS.equals(result2.getCode())) {
-                // 注册成功弹窗 显示服务器返回的信息
-                alert = new Alert(Alert.AlertType.INFORMATION, result2.getMsg());
-                ClientApp.showView(LoginView.class);
-                alert.showAndWait();
-            } else {
-                // 注册失败 弹出错误窗口
-                alert = new Alert(Alert.AlertType.ERROR, result2.getMsg());
-                alert.show();
-                // 错误的话得重新输入
-            }
+            Task<Void> task = new Task<Void>(){
+                @Override
+                protected Void call() throws Exception {
+                    try {
+                        Result result = new Result();
+                        result.setCode(Code.USER_REGISTER);
+                        User user = new User();
+
+                        Result result2 = poolExecutor.submit(() -> {
+                            user.setAccount(account);
+                            user.setPassword(password);
+                            user.setUsername(username);
+                            user.setLogin(0);
+                            result.setObject(user);
+                            Result result3 = null;
+                            try {
+                                result3 = userService.userRegister(result);
+                                UserMemory.users = JSON.parseArray(result3.getObject().toString(), User.class);
+                            } catch (Exception e) {
+                                log.error(e.toString());
+                            }
+                            return result3;
+                        }).get();
+                        Platform.runLater(() -> {
+                            try {
+                                if (Code.REGISTER_SUCCESS.equals(result2.getCode())) {
+                                    // 注册成功弹窗 显示服务器返回的信息
+                                    Alert alert = new Alert(Alert.AlertType.INFORMATION, result2.getMsg());
+                                    ClientApp.showView(LoginView.class);
+                                    alert.showAndWait();
+                                } else {
+                                    // 注册失败 弹出错误窗口
+                                    Alert alert = new Alert(Alert.AlertType.ERROR, result2.getMsg());
+                                    alert.show();
+                                    // 错误的话得重新输入
+                                }
+                            } catch (Exception e) {
+                                log.error(e.toString());
+                            }
+                        });
+                    } catch (Exception e) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR, "未知错误");
+                        alert.show();
+                        log.error(e.toString());
+                    }
+                    return null;
+                }
+            };
+            ProgressStageUtil.of(primaryStage, poolExecutor, task, "注册中").show();
         } else if ("".equals(account)) {
             accountInput.validate();
         } else if ("".equals(password)) {
