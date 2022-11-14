1c8538e1001851bb23b7bf98f6c01b1dbb5242a7
giv <opnmsl@gugoole.com>
2022-11-14 17:48:22 +0800

    离线信息

--- client/src/main/java/com/client/controller/ChatInterface.java
+++ client/src/main/java/com/client/controller/ChatInterface.java
@@ -1,22 +1,49 @@
 package com.client.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.client.ClientApp;
+import com.client.pojo.Code;
+import com.client.pojo.Msg;
+import com.client.pojo.Result;
+import com.client.pojo.User;
+import com.client.service.MsgService;
+import com.client.service.UserService;
+import com.client.utils.UserMemory;
 import com.client.view.UserView;
 import de.felixroske.jfxsupport.FXMLController;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.concurrent.ThreadPoolExecutor;
 
 
 @FXMLController
+@Slf4j
 public class ChatInterface implements Initializable {
 
+    @FXML
+    public TextArea inputArea;
+    @Autowired
+    private MsgService msgService;
+    @Autowired
+    private ThreadPoolExecutor poolExecutor;
+    @Autowired
+    private UserService userService;
     @FXML
     public Label userName;
     public Stage primaryStage;
@@ -35,7 +62,74 @@ public class ChatInterface implements Initializable {
         });
     }
 
-    public void sendMessage(ActionEvent mouseEvent) {
+    public void sendMessage(ActionEvent mouseEvent) throws IOException {
+
+        String text = inputArea.getText();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//注意月和小时的格式为两个大写字母
+        java.util.Date date = new Date();//获得当前时间
+        String mstime = df.format(date);//将当前时间转换成特定格式的时间字符串，这样便可以插入到数据库中
+        Integer contentType= 0;
+//        File file = new File(text);
+//        if(file.isFile()){
+//             contentType=1;
+//        }else {
+//             contentType=0;
+//        }
+
+         Integer senderId = UserMemory.myUser.getId();
+         Integer receiveId = UserMemory.talkUser.getId();
+        if(!text.equals("")){
+            try {
+                Msg msg = new Msg();
+                Result result = new Result();
+                if (1==(UserMemory.talkUser.getLogin())){
+                    result.setCode(Code.SEND_TEXT_MSG);
+                }else {
+                    result.setCode(Code.SEND_OFFLINE_TEXT_MSG);
+                }
+                Result result2 = poolExecutor.submit(() -> {
+                    msg.setMessageTime(mstime);
+                    msg.setContent(text);
+                    msg.setContentType(contentType);
+                    msg.setSenderId(senderId);
+                    msg.setReceiveId(Integer.valueOf(receiveId));
+                    result.setObject(msg);
+                    Result result3 = null;
+                    try {
+                        result3 = msgService.sendMsgByServer(result);
+                        UserMemory.users = JSON.parseArray(result3.getObject().toString(), User.class);
+                    } catch (Exception e) {
+                        log.error(e.toString());
+                    }
+                    return result3;
+                }).get();
+                Platform.runLater(() -> {
+                    try {
+                        if (Code.SEND_OFFLINE_TEXT_MSG_SUCCESS.equals(result2.getCode())) {
+                            // 发送成功弹窗 显示服务器返回的信息
+                            Alert alert = new Alert(Alert.AlertType.INFORMATION, result2.getMsg());
+                            alert.showAndWait();
+                        } else {
+                            // 发送失败 弹出错误窗口
+                            Alert alert = new Alert(Alert.AlertType.ERROR, result2.getMsg());
+                            alert.show();
+                            // 失败的话得重新输入
+                        }
+                    } catch (Exception e) {
+                        log.error(e.toString());
+                    }
+                });
+
+           }catch (Exception e){
+                Alert alert = new Alert(Alert.AlertType.ERROR, "未知错误");
+                alert.show();
+                log.error(e.toString());
+            }
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "发送的消息不能为空！");
+            alert.show();
+
+        }
     }
 
     public void returnToUser(ActionEvent event) {
--- client/src/main/java/com/client/pojo/Msg.java
+++ client/src/main/java/com/client/pojo/Msg.java
@@ -7,7 +7,7 @@ public class Msg {
     private Integer id;
     private String messageTime;
     private String content;
-    private String contentType;
+    private Integer contentType;
     private Integer senderId;
     private Integer receiveId;
 }
